ac766bfc0959689cf341b5267d33966f5b404dac
Glsme <glasses.str.man@gmail.com>
2022-07-28 16:40:33 +0900

    Feat: 연산 프로퍼티 및 프로퍼티 감시자 사용 ( 추후 버그 수정 예정 )

--- AutoExchangeRateProgram/main.swift
+++ AutoExchangeRateProgram/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+struct ExchangeRate {
+    var currentRate: Double {
+        willSet {
+            print("환율 변동 예정 \(currentRate) -> \(newValue)")
+        }
+        
+        didSet {
+            print("환율 변동 완료 \(oldValue) -> \(currentRate)")
+        }
+    }
+    
+    var USD: Double {
+        willSet {
+            print("환전 금액 USD \(newValue)달러로 환전 예정.")
+        }
+        
+        didSet {
+            print("KRW\(self.KRW)원 -> \(oldValue)로 환전 완료.")
+        }
+    }
+    
+    var KRW: Double {
+        get {
+            return 
+        }
+        
+        set {
+            self.USD = newValue / self.currentRate
+        }
+    }
+}
 
+var rate = ExchangeRate(currentRate: 1100, USD: 1)
+
+rate.KRW = 500000
+//rate.currentRate = 1350
+rate.KRW = 50000
